db44e4165a2e50f58cf22813e56e4f44a24ede4d
José Andrés Trujillo <trujillohenaresj@gmail.com>
2023-11-18 14:31:30 +0100

    change k8s_cluster.yml to k8s-cluster.yml

--- modules/cluster_data.rb
+++ modules/cluster_data.rb
@@ -1,16 +1,16 @@
 module ClusterData
     def self.get_cluster_data
-        if !File.exist?("k8s_cluster.yml")
-            File.open("k8s_cluster.yml", "w") do |file|
+        if !File.exist?("k8s-cluster.yml")
+            File.open("k8s-cluster.yml", "w") do |file|
               file.puts({ "nodes" => [
                 { "name": "master", "master": true, "memory": 2048, "cpus": 2 },
                 { "name": "worker", "master": false, "memory": 2048, "cpus": 2 }
               ] }.to_yaml)
             end
           
-            k8s_cluster_data = YAML.load_file("k8s_cluster.yml")
+            k8s_cluster_data = YAML.load_file("k8s-cluster.yml")
           else
-            k8s_cluster_data = YAML.load_file("k8s_cluster.yml")
+            k8s_cluster_data = YAML.load_file("k8s-cluster.yml")
           
             if !k8s_cluster_data.has_key?("nodes")
               k8s_cluster_data["nodes"] = [
@@ -18,11 +18,11 @@ module ClusterData
                 { "name": "worker", "master": false, "memory": 2048, "cpus": 2 }
               ]
           
-              File.open("k8s_cluster.yml", "w") do |file|
+              File.open("k8s-cluster.yml", "w") do |file|
                 file.puts k8s_cluster_data.to_yaml
               end
           
-              k8s_cluster_data = YAML.load_file("k8s_cluster.yml")
+              k8s_cluster_data = YAML.load_file("k8s-cluster.yml")
             end
           end
 
